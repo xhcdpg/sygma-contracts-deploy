fcb35740c8b84e76b52823c8c74c718f17299ec6
chinyuchan <chinyuchan@163.com>
2024-05-21 10:04:51 +0800

    update.

--- ignition/modules/deployAccessControl.js
+++ ignition/modules/deployAccessControl.js
@@ -3,7 +3,12 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("AccessModule", (m) => {
     const c = m.contract("AccessControlSegregator",
-        [["0x80ae1c28"],["0xcCE8B4bc59Ff275fC232b59fF154742dA9ab4E82"]], {});
+        [
+            ["0x80ae1c28", "0x9e84dd0b"], // 一组函数签名: 每一项为函数的keccak256签名取前4字节
+            ["0xcCE8B4bc59Ff275fC232b59fF154742dA9ab4E82", "0xcCE8B4bc59Ff275fC232b59fF154742dA9ab4E82"] // 一组地址
+            // 函数签名数组的长度和地址数组长度必须相等，即为每个地址对应一个函数签名
+        ],
+        {});
 
     return { c };
 });
--- ignition/modules/deployBridge.js
+++ ignition/modules/deployBridge.js
@@ -2,7 +2,12 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("BridgeModule", (m) => {
     const lock = m.contract("Bridge",
-        [1,"0x9f47daF2343f4329e109dEa0a19E546dD3f2f243"], {});
+        [
+            1, // 自己指定的ID，而非真正的chainID 比如：findora为1，goerli为2
+            "0x9f47daF2343f4329e109dEa0a19E546dD3f2f243" // AccessControl合约地址
+        ],
+        {});
 
     return { lock };
 });
+
